5fc091b3f1412bd49b7110cd471e8f877ca1f512
ravi M <104335320+ravIm2022@users.noreply.github.com>
2023-04-21 01:42:41 +0530

    Update app.js
     removed console.log proces.env

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-console.log(process.env.MONGO_URI);
+
 // mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 const conn = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
